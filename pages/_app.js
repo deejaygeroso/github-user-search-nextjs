6809144aba4d44f331020c5d21620221321b06f8
Deejay Geroso <deejaygeroso@gmail.com>
2019-02-16 10:06:51 +0800

    added page transitions

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,35 @@
-import React from 'react';
-import {Provider} from "react-redux";
-import App, { Container } from 'next/app';
-import Head from 'next/head';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import JssProvider from 'react-jss/lib/JssProvider';
-import getPageContext from '../src/getPageContext';
+import React from "react";
+import { Provider } from "react-redux";
+import App, { Container } from "next/app";
+import Head from "next/head";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import JssProvider from "react-jss/lib/JssProvider";
+import getPageContext from "../src/getPageContext";
 import withRedux from "next-redux-wrapper";
-import {initStore} from "../src/store";
+import { initStore } from "../src/store";
+import { PageTransition } from "next-page-transitions";
 
 class MyApp extends App {
-  
   constructor() {
     super();
     this.pageContext = getPageContext();
   }
 
-  static async getInitialProps({Component, ctx}) {
+  static async getInitialProps({ Component, ctx }) {
     return {
-        pageProps: {
-            // Call page-level getInitialProps
-            ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
-        }
+      pageProps: {
+        // Call page-level getInitialProps
+        ...(Component.getInitialProps
+          ? await Component.getInitialProps(ctx)
+          : {})
+      }
     };
-}
+  }
 
   componentDidMount() {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles && jssStyles.parentNode) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
@@ -37,32 +39,52 @@ class MyApp extends App {
     const { Component, pageProps, store } = this.props;
     return (
       <Container>
-               <Provider store={store}>
-        <Head>
-          <title>My page</title>
-        </Head>
-        {/* Wrap every page in Jss and Theme providers */}
-        <JssProvider
-          registry={this.pageContext.sheetsRegistry}
-          generateClassName={this.pageContext.generateClassName}
-        >
-          {/* MuiThemeProvider makes the theme available down the React
-              tree thanks to React context. */}
-          <MuiThemeProvider
-            theme={this.pageContext.theme}
-            sheetsManager={this.pageContext.sheetsManager}
+        <Provider store={store}>
+          <Head>
+            <title>My page</title>
+          </Head>
+          {/* Wrap every page in Jss and Theme providers */}
+          <JssProvider
+            registry={this.pageContext.sheetsRegistry}
+            generateClassName={this.pageContext.generateClassName}
           >
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            {/* Pass pageContext to the _document though the renderPage enhancer
+            {/* MuiThemeProvider makes the theme available down the React
+              tree thanks to React context. */}
+            <MuiThemeProvider
+              theme={this.pageContext.theme}
+              sheetsManager={this.pageContext.sheetsManager}
+            >
+              {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+              <CssBaseline />
+              {/* Pass pageContext to the _document though the renderPage enhancer
                 to render collected styles on server-side. */}
-            <Component pageContext={this.pageContext} {...pageProps} />
-          </MuiThemeProvider>
-        </JssProvider>
+              <PageTransition timeout={300} classNames="page-transition">
+                <Component pageContext={this.pageContext} {...pageProps} />
+              </PageTransition>
+            </MuiThemeProvider>
+          </JssProvider>
         </Provider>
+        <style jsx global>{`
+          .page-transition-enter {
+            opacity: 0;
+            transform: translate3d(0, 20px, 0);
+          }
+          .page-transition-enter-active {
+            opacity: 1;
+            transform: translate3d(0, 0, 0);
+            transition: opacity 300ms, transform 300ms;
+          }
+          .page-transition-exit {
+            opacity: 1;
+          }
+          .page-transition-exit-active {
+            opacity: 0;
+            transition: opacity 300ms;
+          }
+        `}</style>
       </Container>
     );
   }
 }
 
-export default withRedux(initStore, {debug: true})(MyApp)
+export default withRedux(initStore, { debug: true })(MyApp);
