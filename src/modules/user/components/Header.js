34bb2e20b9ff5681dd64486e75d56fb2536f6177
Deejay Geroso <deejaygeroso@gmail.com>
2019-02-16 08:26:01 +0800

    added back button

--- src/modules/user/components/Header.js
+++ src/modules/user/components/Header.js
@@ -5,37 +5,56 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import Avatar from '@material-ui/core/Avatar';
 import red from '@material-ui/core/colors/red';
-import CardActionArea from '@material-ui/core/CardActionArea';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import Button from '@material-ui/core/Button';
+import Router from 'next/router'
 
 const styles = {
   card: {
-    maxWidth: 400,
+    display: 'flex',
+    flexDirection: 'row',
+    borderRadius: 0
   },
   avatar: {
     backgroundColor: red[500],
   },
+  cardHeader: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 0
+  },
+  iconHover: {
+    fontSize: 50,
+    margin: 0,
+  },
+  button: {
+    padding: 0,
+    margin: 0,
+  }, 
 };
 
 class Header  extends React.Component {
   state = { expanded: false };
 
-  handleExpandClick = () => {
-    this.setState(state => ({ expanded: !state.expanded }));
+  goBack = () => {
+      Router.back()
   };
 
   render() {
-    const { classes, user, onClick } = this.props;
+    const { classes, user } = this.props;
     return (
       <Card className={classes.card}>
-      <CardActionArea onClick={onClick}>
+        <Button onClick={this.goBack} className={classes.button}>
+          <KeyboardArrowLeft className={classes.iconHover} />
+        </Button>
         <CardHeader
           avatar={
             <Avatar src={user.avatar_url} aria-label="Recipe" className={classes.avatar} />
           }
           title={user.login}
           subheader={user.html_url}
+          className={classes.cardHeader}
         />
-        </CardActionArea>
       </Card>
     );
   }
@@ -43,7 +62,6 @@ class Header  extends React.Component {
 
 Header .propTypes = {
   user : PropTypes.object,
-  onClick : PropTypes.func,
   classes: PropTypes.object.isRequired,
 };
 
